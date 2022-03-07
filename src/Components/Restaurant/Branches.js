cb45fd8df2987c5106f66699bf85f44ace3b49e6
zahra <alzahraam209@gmail.com>
2022-03-07 03:43:17 +0200

    static branches tap

--- src/Components/Restaurant/Branches.js
+++ src/Components/Restaurant/Branches.js
@@ -1,18 +1,42 @@
-const Branches = () =>{
-    return(
-        <>
-        <div class="tab-content" id="myTabContent">
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../About.scss";
+const Branches = () => {
+  return (
+    <>
+      <div class="tab-content" id="myTabContent">
         <div
           class="tab-pane fade show active"
           id="Branches"
           role="tabpanel"
           aria-labelledby="home-tab"
         >
-          Branches
+          <div className="container-fluid">
+            <div className="d-flex flex-row justify-content-between">
+              <div>
+                <h5>Branches</h5>
+              </div>
+              <div>
+                <p>Count of branches</p>
+              </div>
+            </div>
+            <div className="d-flex flex-column">
+              <ul>
+                <li>
+                  <h4>Branch Name</h4>
+                  <p>- Area Name</p>
+                  <FontAwesomeIcon icon={faClock} />{" "}
+                  <span className="aboutDivText"> Working hours </span>
+                  <p>- Area Name</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <hr className="m-3 text-secondary" />
         </div>
       </div>
-        </>
-    )
-}
+    </>
+  );
+};
 
 export default Branches;
